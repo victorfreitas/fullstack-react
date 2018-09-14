3617d22fdfee1a5490b2793bdc20ad9b5a8b5d63
Victor Freitas <victorfreitas1@gmail.com>
2018-09-14 02:17:25 -0300

    Add like and unlike router

---
+++ backend/src/controllers/posts/likePost.js
@@ -0,0 +1,34 @@
+import Posts from './Posts'
+import isEmpty from '../../helpers/isEmpty'
+
+class LikePost extends Posts {
+  process() {
+    return this.Model.findOne({
+      _id: this.params.id,
+      likes: {
+        $elemMatch: { user: this.user.id },
+      },
+    }).select(['_id'])
+  }
+
+  addLike() {
+    return this.Model.findOneAndUpdate(
+      { _id: this.params.id },
+      { $push: { likes: { user: this.user.id } } },
+      { new: true }
+    ).select(['likes', '-_id'])
+  }
+
+  success(post) {
+    if (!isEmpty(post)) {
+      this.res.status(400).json({ error: 'Your already liked this post' })
+      return
+    }
+
+    this.addLike()
+      .then(likes => this.res.json(likes))
+      .catch(err => this.fail(err.message))
+  }
+}
+
+export default (req, res) => new LikePost(req, res)
--- backend/src/routes/api/posts.js
+++ backend/src/routes/api/posts.js
@@ -6,6 +6,8 @@ import createPost from '../../controllers/posts/createPost'
 import getPosts from '../../controllers/posts/getPosts'
 import getPostById from '../../controllers/posts/getPostById'
 import removePost from '../../controllers/posts/removePost'
+import likePost from '../../controllers/posts/likePost'
+import unlikePost from '../../controllers/posts/unlikePost'
 
 const router = Router()
 
@@ -36,5 +38,19 @@ router.get('/:id', getPostById)
  * @access Public
  */
 router.delete('/:id', auth(), removePost)
+
+/**
+ * @route  POST api/posts/:id/like
+ * @desc   Like post
+ * @access Private
+ */
+router.post('/:id/like', auth(), likePost)
+
+/**
+ * @route  POST api/posts/:id/unlike
+ * @desc   Unlike post
+ * @access Private
+ */
+router.post('/:id/unlike', auth(), unlikePost)
 
 export default router
